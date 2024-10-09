72c5a262a66eef5937c219e16089893ca0d09365
fourat <fourat.chiha@esprit.tn>
2024-10-09 16:45:32 +0100

    reservation

---
+++ src/main/java/com/example/projetdevops/ProjetdevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetdevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetdevopsApplication.class, args);
+    }
+
+}
