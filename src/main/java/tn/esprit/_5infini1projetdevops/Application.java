dce130078be03a5bd6684a6f45bf86c355cf7082
Rahmaml <rahma.moalla@esprit.tn>
2024-10-26 13:14:25 +0100

    nw branch

---
+++ src/main/java/tn/esprit/_5infini1projetdevops/Application.java
@@ -0,0 +1,13 @@
+package tn.esprit._5infini1projetdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/tn/esprit/_5infini1projetdevops/Controllers/EtudiantRestControllor.java
@@ -0,0 +1,49 @@
+package tn.esprit._5infini1projetdevops.Controllers;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit._5infini1projetdevops.Entity.Etudiant;
+import tn.esprit._5infini1projetdevops.services.EtudiantService;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/etudiant")
+public class EtudiantRestControllor {
+
+    public EtudiantService etudiantService ;
+
+    @GetMapping("/show-etudiant")
+    public List<Etudiant> getAlletudiants() {
+        List<Etudiant> listEtudiants = etudiantService.findAll();
+        return listEtudiants;
+    }
+
+    @GetMapping("/show-etudiantid/{id}")
+    public List<Etudiant> getEtudiant(@PathVariable("id") Long idEtudiant)
+    {
+        return (List<Etudiant>) etudiantService.findById(idEtudiant);
+    }
+
+    @PostMapping(path = "/add-etudiant")
+    public Etudiant addEtudiant (@RequestBody Etudiant etudiant)
+    {
+        return etudiantService.addOrUpdate(etudiant);
+    }
+
+    @DeleteMapping(path = "/delete-etudiant/{id}")
+    public void deleteEtudiant (@PathVariable ("id") Long idEtudiant)
+    {
+        etudiantService.deleteById(idEtudiant);
+    }
+
+    @PutMapping(path = "/update-etudiant")
+    public Etudiant updateBloc(@RequestBody Etudiant e )
+    {
+
+        Etudiant etudiant = etudiantService.addOrUpdate(e);
+        return etudiant ;
+    }
+}
