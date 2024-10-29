39142dcfa216fa7867cbb2027232ebb20d9bd67b
yomnalayouni <80210620+yomnalayouni@users.noreply.github.com>
2024-10-29 14:01:28 +0100

    Update EtudiantServiceTest.java

--- src/test/java/EtudiantServiceTest.java
+++ src/test/java/EtudiantServiceTest.java
@@ -18,7 +18,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class EtudiantServiceTest {
+class EtudiantServiceTest {
 
     @Mock
     private EtudiantRepository repo;
