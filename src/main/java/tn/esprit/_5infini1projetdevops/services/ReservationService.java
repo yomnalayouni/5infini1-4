f35ef5a7556d0e9c6ede4044606a267c4df1192c
fouratch01 <125090524+fouratch01@users.noreply.github.com>
2024-10-29 14:42:05 +0100

    Update ReservationService.java

--- src/main/java/tn/esprit/_5infini1projetdevops/services/ReservationService.java
+++ src/main/java/tn/esprit/_5infini1projetdevops/services/ReservationService.java
@@ -68,19 +68,19 @@ public class ReservationService implements IReservationService {
         }
     }
 
-    @Override
+    
     public Universite getuniversite(Long idUniversite) {
         return universiteRepository.findById(idUniversite)
                 .orElseThrow(() -> new NoSuchElementException("Université with ID " + idUniversite + " not found"));
     }
-   @Override
+   
    public void assignFoyerToUniversite(Long idUniversite, Foyer foyer) {
         Universite universite = universiteRepository.findById(idUniversite)
                 .orElseThrow(() -> new NoSuchElementException("Université not found"));
         universite.setFoyer(foyer);
         universiteRepository.save(universite);
     }
-   @Override
+   
    public void unassignFoyerToUniversite(Long idUniversite) {
         Universite universite = universiteRepository.findById(idUniversite)
                 .orElseThrow(() -> new NoSuchElementException("Université not found"));
